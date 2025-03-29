dbffaaf5f58ba7f3c58f845a6f21d13c9761c54b
thapabishnu <thapabisnu22@gmail.com>
2025-03-29 08:44:44 +0000

    fixes in docker-compose yaml file

--- MyTodoProject/settings.py
+++ MyTodoProject/settings.py
@@ -79,7 +79,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tododb',
         'USER': 'postgres',
-        'HOST': '192.168.10.3'
+        'PASSWORD': 'postgres',
+        #'HOST': '192.168.10.3'
+        'HOST': 'mytodoproject-db-1'
     }
 }
 
